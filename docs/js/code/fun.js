45455ea17c99b5925b2bb8892cce17cf0777f9ce
Carlos Aleman <caleman9791@protonmail.com>
2023-05-23 07:07:44 -0400

    ok

--- docs/js/code/fun.js
+++ docs/js/code/fun.js
@@ -1,8 +1,16 @@
+var URERS = [];
+var COOKE = [];
+var URER = {
+	nombre: "",
+};
+
+
 function elimina_nodos(contenedor) {
 	while (contenedor.firstChild) {
 		contenedor.removeChild(contenedor.firstChild);
 	}
 }
+
 
 function crearCookie(nombre, valor, diasExpiracion) {
 	var fecha = new Date();
@@ -36,18 +44,8 @@ function getCookieValue(nombreCookie) {
 			return JSON.parse(obj);
 		}
 	}
-	return null;
+	return false;
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-// function obtenerUltimoValorURLSinExtension() {
-// 	var url = window.location.href; // Obtener la URL actual
-// 	var partes = url.split('/'); // Dividir la URL por "/"
-// 	var ultimoValorConExtension = partes[partes.length - 1]; // Obtener el último valor con extensión
-// 	var ultimoValor = ultimoValorConExtension.split('.')[0]; // Eliminar la extensión
-// 	return ultimoValor;
-// }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-// //////////////////////////////////////////////////////////
 // Función para guardar una matriz de objetos en localStorage
 function guardarMatrizEnLocalStorage(clave, matriz) {
 	localStorage.setItem(clave, JSON.stringify(matriz));
@@ -57,21 +55,7 @@ function recuperarMatrizDeLocalStorage(clave) {
 	var matrizString = localStorage.getItem(clave);
 	return JSON.parse(matrizString);
 }
-// var miMatriz = [{
-// 	nombre: "Objeto 1",
-// 	valor: 10
-// }, {
-// 	nombre: "Objeto 2",
-// 	valor: 20
-// }, {
-// 	nombre: "Objeto 3",
-// 	valor: 30
-// }];
-// Guardar la matriz en localStorage
-// guardarMatrizEnLocalStorage("miMatriz", miMatriz);
-// // Recuperar la matriz de localStorage
-// var matrizRecuperada = recuperarMatrizDeLocalStorage("miMatriz");
-// console.log(matrizRecuperada);	
+
 function insertaUsuario(nombre, email, pass) {
 	URERS = [];
 	if (recuperarMatrizDeLocalStorage("USERS")) {
@@ -100,114 +84,66 @@ function insertaUsuario(nombre, email, pass) {
 		let $data = recuperarMatrizDeLocalStorage("USERS");
 		URERS = [];
 		for (var i = 0; i < $data.length; i++) {
-			if (typeof $data[i].nombre == "string"
-				&& typeof $data[i].email == "string"
-				&& typeof $data[i].email == "string") {
+			if (typeof $data[i].nombre == "string" && typeof $data[i].email == "string" && typeof $data[i].email == "string") {
 				URERS.push($data[i]);
 			}
 		}
 		guardarMatrizEnLocalStorage("USERS", URERS);
 		$data = recuperarMatrizDeLocalStorage("USERS");
-		console.log($data);
+		// console.log($data);
 		crearCookie("USER", JSON.stringify(data), 5);
 		// window.location.href = "./index.html";
 		return true;
 	} else {
 		return false;
-		// console.log("existe");
+		console.log("existe");
 	}
 };
 
-function inicioSecion(email, pass) {
-	URERS = [];
-
-	if (recuperarMatrizDeLocalStorage("USERS")) {
-		URERS = recuperarMatrizDeLocalStorage("USERS");
-	}
-
-	if (URER.length >= 1) {
-		// //////////////////////////////////////
-		for (var i = 0; i < URERS.length; i++) {
-
-			if (URERS[i].email == email && URERS[i].pass == pass) {
-
-				URER = {
-					nombre: USERS.nombre,
-					email: USERS.email,
-					// pass: USERS.pass,
-				};
-
-			}
-
-		}
-		// ///////////////////////////////////////
-
-	}
-}
-
-function inicioSecion(email, pass) {
+function registro(email, pass) {
 	URERS = [];
 	if (recuperarMatrizDeLocalStorage("USERS")) {
 		URERS = recuperarMatrizDeLocalStorage("USERS");
 	}
-	URER = false;
-	for (var i = 0; i < URERS.length; i++) {
-		if (URERS[i].email == email && URERS[i].pass == pass) {
-			URER = {
-				nombre: USERS.nombre,
-				email: USERS.email,
-				// pass: USERS.pass,
-			};
+	// console.log(USERS);
+}
+
+function inicioSecion(email, pass) {
+	console.log(email);
+	console.log(pass);
+	URERS = {};
+	if (recuperarMatrizDeLocalStorage("USERS")) {
+		URERS = recuperarMatrizDeLocalStorage("USERS");
+		// console.log(URERS);
+		console.log(URERS.length);
+		for (var i = 0; i < USERS.length; i++) {
+			console.log(URERS[i]);
 		}
-	}
-	if (URER) {
-		crearCookie("USER", JSON.stringify(URER), 5);
-		if (recuperarMatrizDeLocalStorage("USERS")) {
-			URERS = recuperarMatrizDeLocalStorage("USERS");
-		} else {
-			URERS = [];
-		}
-		URERS.push(data);
-		guardarMatrizEnLocalStorage("USERS", URERS);
-		// URERS = recuperarMatrizDeLocalStorage("USERS");
-		let $data = recuperarMatrizDeLocalStorage("USERS");
-		URERS = [];
-		for (var i = 0; i < $data.length; i++) {
-			if (typeof $data[i].nombre == "string"
-				&& typeof $data[i].email == "string"
-				&& typeof $data[i].email == "string") {
-				URERS.push($data[i]);
-			}
-		}
-		guardarMatrizEnLocalStorage("USERS", URERS);
-		$data = recuperarMatrizDeLocalStorage("USERS");
-		console.log($data);
-		crearCookie("USER", JSON.stringify(data), 5);
-		// window.location.href = "./index.html";
-		return true;
-	} else {
-		// window.location.href = "./register.html";
-		// console.log("existe");
 	}
 }
-// //////////////////////////////////////////////////////////
+
 function agregarScript(url) {
-	let script = document.createElement('script'); // Crear el elemento <script>
-	let fun_script = document.createElement('script'); // Crear el elemento <script>
-	fun_script.src = `./js/fun/js.js`;
-	script.src = `./js/${url}.js`;
-	console.log(script.src); // Asignar la URL al atributo src del elemento <script>
-	document.body.appendChild(fun_script);
-	document.body.appendChild(script); // Agregar el elemento <script> al <body> del documento
+	let script = document.createElement('script');
+	script.src = `./js/main/${url}.js`;
+	// console.log(script.src);
+	document.head.appendChild(script);
 }
-// var nodoEliminar = document.getElementById("miNodo");
-// // Obtener una referencia al padre del nodo
-// var padreNodo = nodoEliminar.parentNode;
-// // Eliminar el nodo del árbol DOM
-// padreNodo.removeChild(nodoEliminar);
-// 
-// function elimina_nodo(nodo) {
-// 	var padreNodo = nodoEliminar.parentNode;
-// 	padreNodo.removeChild(nodoEliminar);
-// }
-//
+
+function pagina() {
+	var url = window.location.href; // Obtener la URL actual
+	var partes = url.split('/'); // Dividir la URL por "/"
+	var ultimoValorConExtension = partes[partes.length - 1]; // Obtener el último valor con extensión
+	var ultimoValor = ultimoValorConExtension.split('.')[0]; // Eliminar la extensión
+	return ultimoValor;
+}
+
+function obtenerUltimoValorURLSinExtension() {
+	var url = window.location.href; // Obtener la URL actual
+	var partes = url.split('/'); // Dividir la URL por "/"
+	var ultimoValorConExtension = partes[partes.length - 1]; // Obtener el último valor con extensión
+	var ultimoValor = ultimoValorConExtension.split('.')[0]; // Eliminar la extensión
+	return ultimoValor;
+}
+// let ultimoValor = obtenerUltimoValorURLSinExtension();
+// //////////////////////////////////////////////////////////////////////////////////////
+// localStorage.clear();
