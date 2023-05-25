f7085a6791c25daf4b954134af86db2d77b55321
Carlos Aleman <caleman9791@protonmail.com>
2023-05-25 19:22:23 -0400

    Eliminado

---
+++ web/js/index/index.js
@@ -0,0 +1,87 @@
+function getPage() {
+	let url = window.location.href; // Obtener la URL actual
+	let partes = url.split('/'); // Dividir la URL por "/"
+	let ultimoValorConExtension = partes[partes.length - 1]; // Obtener el último valor con extensión
+	let ultimoValor = ultimoValorConExtension.split('.')[0]; // Eliminar la extensión
+	return ultimoValor;
+}
+
+// function getPartes() {
+// 	var url = window.location.href; // Obtener la URL actual
+// 	// let partes = url.split('/'); // Dividir la URL por "/"
+// 	var partes = url.split("file:///C:/wamp64/www");
+// 	// console.log(nueva);
+// 	var nuevaurl = "http://localhost/";
+// 	for (var i = 0; i < partes.length; i++) {
+// 		nuevaurl + partes[i];
+// 	}
+// 	console.log(nuevaurl);
+
+// }
+// getPartes();
+// alert("message?: DOMString");
+// console.log(getPage());
+
+
+let pag = getPage();
+if (pag != "index") {
+	window.location = "./index.html";
+
+}
+
+function localhost(arguments) {
+	var urlCompleta = window.location.href;
+	var nuevaURL = urlCompleta.replace("file:///C:/wamp64/www", "http://localhost");
+
+	if (window.location.href != nuevaURL) {
+		if (nuevaURL.includes("localhost")) {
+			window.location.href = nuevaURL;
+		}
+
+	}
+
+}
+// crearCookie("USER", JSON.stringify({
+// 	nombre: "carlos",
+// 	correo: "mail@mail.com"
+// }), 30);
+
+// let x = document.querySelectorAll("script");
+// console.log(x);
+// for (var i = 0; i < x.length; i++) {
+// 	console.log(x[i].href);
+// }
+localhost();
+window.onload = (event) => {
+	let cerrar_sesion = document.getElementById("cerrar_sesion");
+	cerrar_sesion.addEventListener("click", function (e) {
+		e.preventDefault();
+		eliminarCookie("USER");
+		if (!(existeCookie("USER"))) {
+			window.location.reload();
+		}
+	});
+	// alert("message?: DOMString");
+	let x = document.querySelectorAll("script");
+	console.log(x);
+	for (var i = 0; i < x.length; i++) {
+		console.log(x[i].src);
+	}
+};
+// console.log(urlCompleta.includes("localhost"));
+// console.log(nuevaURL.includes("localhost"));
+// // console.log(nuevaURL);
+// // console.log(window.location.ancestorOrigins);
+// console.log(window.location.href);
+// // console.log(window.location.protocol);
+// console.log(window.location.host);
+// // console.log(window.location.port);
+// // console.log(window.location.pathname);
+// // console.log(window.location.search);
+// // console.log(window.location.hash);
+// // console.log(window.location.origin);
+// // // console.log(window.location.xxxxxxxx);
+// // // console.log(window.location.xxxxxxxx);
+// // // console.log(window.location.xxxxxxxx);
+// // // console.log(window.location.xxxxxxxx);
+// // // console.log(window.location.xxxxxxxx);
---
+++ web/js/inicio/inicio.js
@@ -0,0 +1,145 @@
+function crearCookie(nombre, valor, diasExpiracion) {
+	var fecha = new Date();
+	fecha.setTime(fecha.getTime() + (diasExpiracion * 24 * 60 * 60 * 1000));
+	var expiracion = "expires=" + fecha.toUTCString();
+	document.cookie = nombre + "=" + valor + ";" + expiracion + ";path=/";
+}
+
+function eliminarCookie(nombre) {
+	document.cookie = nombre + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+function existeCookie(nombre) {
+	var cookies = document.cookie.split(';');
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i].trim();
+		if (cookie.indexOf(nombre + '=') === 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getCookieValue(nombreCookie) {
+	var nombre = nombreCookie + "=";
+	var cookies = document.cookie.split(";");
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i].trim();
+		if (cookie.indexOf(nombre) === 0) {
+			let obj = cookie.substring(nombre.length, cookie.length);
+			return JSON.parse(obj);
+		}
+	}
+	return false;
+}
+
+
+function obtenerUltimoValorURLSinExtension() {
+
+	let url = window.location.href; // Obtener la URL actual
+	let partes = url.split('/'); // Dividir la URL por "/"
+	let ultimoValorConExtension = partes[partes.length - 1]; // Obtener el último valor con extensión
+	let ultimoValor = ultimoValorConExtension.split('.')[0]; // Eliminar la extensión
+	return ultimoValor;
+
+}
+
+
+function inicio_main() {
+	var USER = {
+		nombre: "",
+		correo: ""
+	};
+	if (!(LDB.Collection('USERS'))) {
+		var USUERS = new LDB.Collection('USERS');
+	} else {
+		var USUERS = LDB.Collection('USERS');
+	}
+	// eliminarCookie("USER");
+	if (getCookieValue("USER")) {
+		console.log(getCookieValue("USER"));
+
+	};
+
+
+	if (getCookieValue("USER")) {
+		console.log(getCookieValue("USER"));
+
+	};
+
+	// USUERS.drop();
+	// USUERS = LDB.Collection('USERS');
+
+	// USUERS.find({
+	// 		nombre: "carlos",
+	// 		correo: "mail@mail.com"
+	// 	},
+	// 	function (results) {
+	// 		if (results[0]) {
+	// 			USER = {
+	// 				nombre: results[0].nombre,
+	// 				correo: results[0].correo
+	// 			};
+	// 		} else {
+	// 			USUERS.save({
+	// 					nombre: "carlos",
+	// 					correo: "mail@mail.com"
+	// 				},
+	// 				function (_items) {
+	// 					console.log('New items:', _items);
+	// 				});
+
+	// 		}
+	// 	});
+
+	var btn_inicio = document.getElementById("btn_inicio");
+
+
+	btn_inicio.addEventListener("click", function (e) {
+		e.preventDefault();
+		var txt_nombre = document.getElementById("exampleInputUsername1");
+		var txt_email = document.getElementById("exampleInputEmail1");
+
+		USUERS.find({
+				nombre: txt_nombre.value,
+				correo: txt_email.value
+			},
+			function (results) {
+				if (results[0]) {
+					USER = {
+						nombre: results[0].nombre,
+						correo: results[0].correo
+					};
+				} else {
+					USER = {
+						nombre: "",
+						correo: ""
+					};
+
+				}
+			});
+
+		if (USER.name != "") {
+			crearCookie("USER", JSON.stringify({
+				nombre: txt_nombre.value,
+				correo: txt_email.value
+			}), 30);
+			if (existeCookie("USER")) {
+				console.log("COOKE EXISTE");
+				console.log('Inicio');
+				window.location.href = "./index.html";
+			}
+
+
+		} else {
+			console.log('Error');
+		}
+
+	});
+}
+
+window.addEventListener("load", function (arguments) {
+
+	inicio_main();
+
+});
